d2847ca2b05b07e9434e417d1e8a91702c6500cc
radiodja <50482103+radiodja@users.noreply.github.com>
2022-03-20 23:09:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "17830798"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "0ed433ec2beb13bcef46034ae80698bb" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5127296062:AAF21Bik-aXbbInV9VnEYt5s5UBcQbqvx1Q" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
